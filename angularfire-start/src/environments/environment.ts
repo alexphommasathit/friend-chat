407def9eea4d32988b40ff06f2183efbf6db5896
alexphommasathit <alex@dhmagency.com>
2024-10-31 15:20:42 -0400

    Install AngularFire
    testing

--- angularfire-start/src/environments/environment.ts
+++ angularfire-start/src/environments/environment.ts
@@ -1,12 +1,10 @@
-export const environment = {
-  firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDM0JhydzUgvZcDUL1aHs6CU-oXLCpAk8M",
+  authDomain: "friendlychat-854eb.firebaseapp.com",
+  projectId: "friendlychat-854eb",
+  storageBucket: "friendlychat-854eb.appspot.com",
+  messagingSenderId: "768333947409",
+  appId: "1:768333947409:web:3012c25578d3ee28d087b0",
+  measurementId: "G-DS2LRJ8B6W"
 };
